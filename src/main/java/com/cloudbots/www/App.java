c4f5bb226a2872a08e0fb844de259a709f31b519
thelkar <thelkar@dell.com>
2022-02-22 15:37:28 +0000

    adding the jar project into git

---
+++ src/main/java/com/cloudbots/www/App.java
@@ -0,0 +1,13 @@
+package com.cloudbots.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Im trying to learn Maven so that i can deploy in Devops...!!!!!!" );
+    }
+}
